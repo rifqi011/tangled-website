8ab6a2693623d9041d46c7c8609bf454782098c3
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-03-17 19:23:31 +0700

    feat: add swiper integration with custom pagination and images

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,31 @@
 import './bootstrap';
+import Swiper from "swiper";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const swiperElement = document.querySelector(".swiper");
+    const paginationElement = document.querySelector(
+        ".swiper-custom-pagination"
+    );
+
+    if (swiperElement && paginationElement) {
+        new Swiper(swiperElement, {
+            modules: [Autoplay, Pagination],
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            spaceBetween: 10,
+            pagination: {
+                el: paginationElement, // Menggunakan pagination di luar swiper
+                clickable: true,
+                renderBullet: function (index, className) {
+                    return `<span class="${className} custom-bullet"></span>`;
+                },
+            },
+        });
+    }
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,25 @@
+@php
+    $swiperImages = ['/images/swiper-images/1.png', '/images/swiper-images/2.png', '/images/swiper-images/3.png'];
+@endphp
+
 <x-layout>
+    <div class="relative">
+        {{-- Swiper --}}
+        <div class="swiper relative overflow-visible">
+            <div class="swiper-wrapper">
+                @foreach ($swiperImages as $image)
+                    <div class="swiper-slide">
+                        <img src="{{ asset($image) }}" alt="Slide Image" class="h-[80%] w-full rounded-3xl object-cover">
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        {{-- Pagination --}}
+        <div class="swiper-custom-pagination left-0 right-0 my-2 flex justify-center">
+            <span class="swiper-pagination-bullet custom-bullet"></span>
+        </div>
+    </div>
+
     <h1 class="text-3xl font-bold">Tangled</h1>
 </x-layout>
