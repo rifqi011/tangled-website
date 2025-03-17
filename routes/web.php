14350193287f531e1d0be9729498ce5140c6d1c5
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-03-17 20:03:10 +0700

    feat: implement HomeController and update route to use controller method

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::redirect('/home', '/', 301);
