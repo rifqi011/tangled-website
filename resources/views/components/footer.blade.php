0fa9e479b26fcf82000cdbf4ef252ba4128b5a6d
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-03-18 20:00:39 +0700

    Feature: add Instagram and WhatsApp icons to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,13 @@
 <footer class="flex flex-col items-center justify-center w-full mb-24 text-2xl font-medium gap-2">
     <p>&copy; {{ date('Y') }} Tangled. All rights reserved</p>
     <p>Made with ❤️ by <a href="#" class="font-semibold underline">Shifu Tech</a></p>
+    <div class="flex gap-4">
+        <a href="https://instagram.com/shifutechnology" target="_blank">
+            <img src="{{ asset('images/icons/instagram.svg') }}" class="w-8" alt="instagram icon">
+        </a>
+
+        <a href="https://wa.me/62882003598475" target="_blank">
+            <img src="{{ asset('images/icons/whatsapp.svg') }}" class="w-8" alt="whatsapp icon">
+        </a>
+    </div>
 </footer>
