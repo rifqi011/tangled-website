5fc08e92afa24ea71fc95e31df60db0f7938eca0
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-03-18 19:49:06 +0700

    Feature: add footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="flex flex-col items-center justify-center w-full mb-24 text-2xl font-medium gap-2">
+    <p>&copy; {{ date('Y') }} Tangled. All rights reserved</p>
+    <p>Made with ❤️ by <a href="#" class="font-semibold underline">Shifu Tech</a></p>
+</footer>
