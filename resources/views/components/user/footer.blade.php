9fe4c1b23db65787e4037ab1fe2eafac562c9daf
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-04-10 09:07:36 +0700

    Refactor: adjust footer styles for consistency and improved readability

--- resources/views/components/user/footer.blade.php
+++ resources/views/components/user/footer.blade.php
@@ -1,15 +1,15 @@
 <hr class="my-4 bg-black w-[90%] mx-auto">
 
-<footer class="mb-24 flex w-full flex-col items-center justify-center gap-2 text-2xl font-medium">
+<footer class="mb-24 flex w-full flex-col items-center justify-center gap-2 text-lg font-medium">
     <p>&copy; {{ date('Y') }} Tangled. All rights reserved</p>
     <p>Made with ❤️ by <a href="#" class="font-semibold underline">Shifu Tech</a></p>
     <div class="flex gap-4">
         <a href="https://instagram.com/shifutechnology" target="_blank">
-            <img src="{{ asset('images/icons/instagram.svg') }}" class="w-8" alt="instagram icon">
+            <img src="{{ asset('images/icons/instagram.svg') }}" class="w-6" alt="instagram icon">
         </a>
 
         <a href="https://wa.me/62882003598475" target="_blank">
-            <img src="{{ asset('images/icons/whatsapp.svg') }}" class="w-8" alt="whatsapp icon">
+            <img src="{{ asset('images/icons/whatsapp.svg') }}" class="w-6" alt="whatsapp icon">
         </a>
     </div>
 </footer>
