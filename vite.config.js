168fadbf19dd1b04f664988ca945cb55b7f758b9
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-04-10 08:28:08 +0700

    Refactor: standardize quotes and formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        host: "192.168.40.170", // pakai IP LAN dari komputermu, bukan `true`
+        port: 5173,
+        strictPort: true,
+        cors: {
+            origin: "http://192.168.40.170:8000", // Allow dari Laravel
+            credentials: true,
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
