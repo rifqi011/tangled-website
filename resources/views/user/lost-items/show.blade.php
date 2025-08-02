ae682652e1478ffe0bd2d2144b708849b0092984
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-08-02 22:34:59 +0700

    Refactor: Remove unused fields from lost item detail view for clarity

--- resources/views/user/lost-items/show.blade.php
+++ resources/views/user/lost-items/show.blade.php
@@ -1,12 +1,5 @@
 <x-user.item-detail :item="$lostItem" type="lost">
     <x-user.item-info-section :fields="[
-        'Nama' => $lostItem->username,
-        'Kelas' => $lostItem->class->name,
-    ]" />
-
-    <x-user.form.divider />
-
-    <x-user.item-info-section :fields="[
         'Tanggal hilang' => $lostItem->lost_date,
         'Lokasi terakhir' => $lostItem->last_location,
         'Kategori' => $lostItem->category->name,
